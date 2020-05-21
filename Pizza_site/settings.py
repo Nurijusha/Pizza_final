a35115b6002cc43c3ad97b03d8a57d8d13f1f2c3
Nurijusha <48881109+Nurijusha@users.noreply.github.com>
2020-05-21 09:57:27 +0300

    white

--- Pizza_site/settings.py
+++ Pizza_site/settings.py
@@ -60,7 +60,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
